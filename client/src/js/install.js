14df165a8df43cf9eafbb7932ab340b2b4030da6
Tyler Farrior <tylerfarrior@gmail.com>
2023-11-20 16:21:38 -0500

    adding to server.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,18 +1,32 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    butInstall.style.visibility = 'visible';
-    textHeader.textContent = 'Click the button to install!';
-  
-    butInstall.addEventListener('click', () => {
-      event.prompt();
-      butInstall.setAttribute('disabled', true);
-      butInstall.textContent = 'Installed!';
-    });
+
+    // Store the triggered events
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
   });
+
+butInstall.addEventListener('click', async () => {
   
-  window.addEventListener('appinstalled', (event) => {
-    textHeader.textContent = 'Successfully installed!';
-    console.log('👍', 'appinstalled', event);
-  });
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
+}); 
